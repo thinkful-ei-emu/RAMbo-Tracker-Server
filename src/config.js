0f01b062d83bd9096d322d7797cfc193b263496f
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-17 13:42:51 -0500

    made migration consistent

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL:
-    process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET || 'this-is-a-secret',
+  DB_URL: process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET || 'auth-token',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '1h'
 }
