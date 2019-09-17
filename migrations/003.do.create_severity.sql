436809754247718f794b926b4543e64d1e4bd026
David Queen <dsqjr@msn.com>
2019-09-17 16:01:29 -0400

    finished migrations

--- migrations/003.do.create_severity.sql
+++ migrations/003.do.create_severity.sql
@@ -1,14 +1,13 @@
-DROP TABLE IF EXISTs severity ON DELETE CASCADE;
-CREATE TBALE severity(
-  id INTEGER PRIMARY KEY,
+DROP TABLE IF EXISTs severity;
+CREATE TABLE severity (
+  id INT PRIMARY KEY,
   name VARCHAR NOT NULL
 );
 
 INSERT INTO severity(id,name)
-VALUES(
-  (1,"Low"),
-  (2,"Mild"),
-  (3,"Medium"),
-  (4,"High"),
-  (5,"Extreme")
-);
+VALUES
+  (1,'Low'),
+  (2,'Mild'),
+  (3,'Medium'),
+  (4,'High'),
+  (5,'Extreme');
