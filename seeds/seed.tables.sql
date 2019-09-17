b37b940fc341fb7018c63dd236269aa6005f3d06
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-17 11:10:02 -0500

    setup postgrator, config.js, server.js

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -9,7 +9,6 @@ TRUNCATE
 INSERT INTO 'users' ('id', 'username', 'password', 'display_name')
 VALUES
 (
-  1,
   'testuser',
   'pass',
   '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG',
@@ -24,7 +23,6 @@ INSERT INTO 'symptoms' ('id', 'user', 'severity', 'type', 'timestamp')
 VALUES
 (
   1,
-  1,
   4, 
   'bloating',
   1568731573
@@ -34,13 +32,8 @@ INSERT INTO 'meals' ('id', 'user', 'food', 'ingredients', 'timestamp')
 VALUES
 (
   1,
-  1,
   ARRAY [ 'hamburger', 'fries' ],
   ARRAY [ 'wheat', 'beef', 'spices', 'palm oil', 'potato', 'salt' ]
 );
-
-SELECT setval('users_id_seq', (SELECT MAX(id) from "users"));
-SELECT setval('symptoms_id_seq', (SELECT MAX(id) from "symptoms"));
-SELECT setval('meals_id_seq', (SELECT MAX(id) from "meals"));
 
 COMMIT;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  DB_URL:
+    process.env.DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET || 'this-is-a-secret',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '1h'
 }
