c3717e8e5d986bceaab841add65425cd5a921d15
David Queen <dsqjr@msn.com>
2019-09-17 12:12:02 -0400

    edited seed file

---
+++ migrations/003.do.create_severity.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXIST severity ON DELETE CASCADE;
+CREATE TBALE severity(
+  id INT PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
+
+INSERT INTO severity(id,name)
+VALUES(
+  (1,"Low"),
+  (2,"Mild"),
+  (3,"Medium"),
+  (4,"High"),
+  (5,"Extreme")
+);
--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -11,29 +11,24 @@ VALUES
 (
   1,
   'testuser',
-  'pass',
+/*   'pass', */
   '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG',
   'Test User'
 );
 
-INSERT INTO 'severity' ('id')
-VALUES
-(1), (2), (3), (4), (5);
 
-INSERT INTO 'symptoms' ('id', 'user', 'severity', 'type', 'timestamp')
+INSERT INTO 'symptoms' ( 'user', 'severity', 'type', 'timestamp')
 VALUES
 (
-  1,
   1,
   4, 
   'bloating',
   1568731573
 );
 
-INSERT INTO 'meals' ('id', 'user', 'food', 'ingredients', 'timestamp')
+INSERT INTO 'meals' ('user', 'food', 'ingredients', 'timestamp')
 VALUES
 (
-  1,
   1,
   ARRAY [ 'hamburger', 'fries' ],
   ARRAY [ 'wheat', 'beef', 'spices', 'palm oil', 'potato', 'salt' ]
