b917395ada812c4b3716dea705decac4df5845a2
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-17 10:12:34 -0500

    made fixtures less dumb

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,92 +1,72 @@
 const apiFixtures = {
-  user: {
-    post: {
-      req: {
-        username: 'testuser',
-        password: 'pass',
-        display_name: 'Test User'
-      }
-    },
-    get: {
-      res: {
-        username: 'testuser',
-        display_name: 'Test User',
-        events: [
+  postUserReq: {
+    username: 'testuser',
+    password: 'pass',
+    display_name: 'Test User'
+  },
+
+  getUserRes: {
+    username: 'testuser',
+    display_name: 'Test User',
+    events: [
+      {
+        type: 'meal',
+        items: [
           {
-            type: 'meal',
-            items: [
-              {
-                name: 'hamburger',
-                ingredients: ['wheat', 'beef', 'salt', 'spices']
-              },
-              {
-                name: 'fries',
-                ingredients: ['palm oil', 'potato', 'salt']
-              }
-            ],
-            time: 1568727973
+            name: 'hamburger',
+            ingredients: ['wheat', 'beef', 'salt', 'spices']
           },
           {
-            type: 'symptom',
-            symptom: 'bloating',
-            severity: 4,
-            time: 1568731573
+            name: 'fries',
+            ingredients: ['palm oil', 'potato', 'salt']
           }
         ],
-        tracked_symptoms: ['bloating', 'headaches']
-      }
-    }
-  },
-
-  auth: {
-    login: {
-      req: {
-        username: 'testuser',
-        password: 'pass'
+        time: 1568727973
       },
-      res: {
-        token:
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1Njg3MzE5MDcsImV4cCI6MTYwMDI2NzkwNywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsInVzZXJuYW1lIjoiSm9obm55IiwicGFzcyI6IlJvY2tldCJ9.poT0cfI4yQbu28euEdTgLnvmqaH__-OzKphdvjX5vkA'
-      }
-    }
-  },
-
-  event: {
-    post: {
-      reqSymptom: {
+      {
         type: 'symptom',
         symptom: 'bloating',
         severity: 4,
-        time: 1568727973
-      },
-      reqMeal: {
-        type: meal,
-        items: [123123, 234, 2345356, 1345, 4356546],
         time: 1568731573
       }
-    },
+    ],
+    tracked_symptoms: ['bloating', 'headaches']
+  },
 
-    'food/search?term=butter': {
-      get: {
-        res: {
-          total: 123,
-          results: [
-            {
-              offset: 0,
-              name: 'Super Excellent Butter',
-              ndbno: 1231231,
-              manu: 'Excellent Butter Company'
-            },
-            {
-              offset: 1,
-              name: 'Okay butter',
-              ndbno: 123123,
-              manu: 'Mediocre Butter Company'
-            }
-          ]
-        }
+  postAuthLoginReq: {
+    username: 'testuser',
+    password: 'pass'
+  },
+
+  postEventSymptomReq: {
+    type: 'symptom',
+    symptom: 'bloating',
+    severity: 4,
+    time: 1568727973
+  },
+  
+  postEventMealReq: {
+    type: 'meal',
+    items: [123123, 234, 2345356, 1345, 4356546],
+    time: 1568731573
+  },
+
+  getFoodSearchRes: {
+    total: 123,
+    results: [
+      {
+        offset: 0,
+        name: 'Super Excellent Butter',
+        ndbno: 1231231,
+        manu: 'Excellent Butter Company'
+      },
+      {
+        offset: 1,
+        name: 'Okay butter',
+        ndbno: 123123,
+        manu: 'Mediocre Butter Company'
       }
-    }
+    ]
   }
 };
 
