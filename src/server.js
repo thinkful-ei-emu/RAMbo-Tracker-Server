72aa326bc4bb67c000e99c529c1dcd9a94fc8372
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-18 09:14:18 -0500

    fixed server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
-const app = require('./app');
-const {PORT} = require('./config');
+require("dotenv").config();
 
+const app = require('./app');
+const {PORT, DB_URL} = require('./config');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL
+})
+
+app.set('db', db);
 
 app.listen(PORT,()=>{
 console.log(`Server is listening on port ${PORT}`);
