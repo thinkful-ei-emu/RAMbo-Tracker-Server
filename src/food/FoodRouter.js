b128c86359e08fa89110f58561a4a1c20fb01c55
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-26 13:19:48 -0500

    changed 201 to 204 in food router

--- src/food/FoodRouter.js
+++ src/food/FoodRouter.js
@@ -59,14 +59,14 @@ FoodRouter.use(requireAuth)
           return FoodService.addIngredients(req.app.get('db'), ndbno, body);
         })
         .then(() => {
-          res.status(201).end();
+          res.status(204).end();
           next();
         })
         .catch((error) => {
           return res.status(400).json(error);
         });
     } else {
-      res.status(201).end();
+      res.status(204).end();
       next();
     }
   });
