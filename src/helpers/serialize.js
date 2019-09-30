ca4a95cebccd6f4b8acca09b2aee63c6dad8238c
Blake Lowrey <blake.lowrey1@gmail.com>
2019-09-30 11:27:56 -0400

    added serialize helpers

---
+++ src/helpers/serialize.js
@@ -0,0 +1,25 @@
+const xss = require('xss');
+
+function serializeObjectArr(objArr){
+  let responses = [];
+  for (let i = 0 ; i < objArr.length; i++){
+    let myObj = {};
+    let entries = Object.entries(foods[i]);
+    for (let k = 0 ; k < entries.length; k ++){
+      myObj[entries[k][0]] = xss(entries[k][1]);
+    }
+    responses.push(myObj);
+  }
+  return responses;
+}
+
+function serializeObject(obj){
+  let newObj = {};
+  let entries = Object.entries(obj);
+  for (let i = 0 ; i < entries.length; i++){
+    newObj[entries[i][0]] = xss(entries[i][1]);
+  }
+  return newObj;
+}
+
+module.exports = {serializeObjectArr, serializeObject};
