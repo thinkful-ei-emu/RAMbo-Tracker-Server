958c3f25d5ce5acca3fbbab263e917faed417b91
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-30 09:20:27 -0500

    fixed results

--- test/results-endpoints.test.js
+++ test/results-endpoints.test.js
@@ -1,7 +1,7 @@
 const app = require('../src/app');
 const helpers = require('./test-helpers');
 
-describe.only('Results Endpoint', function() {
+describe('Results Endpoint', function() {
   let db;
 
   const testUsers = helpers.makeUsersArray();
