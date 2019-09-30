175cf44a60dd48fa84ef5f762ff4bbe0c654f10b
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-30 15:22:43 -0500

    trying to fix database

--- migrations/007.do.alter_symptoms.sql
+++ migrations/007.do.alter_symptoms.sql
@@ -1,9 +1,11 @@
 INSERT INTO user_symptom (type, user_id) SELECT DISTINCT type, user_id FROM symptoms;
 
-ALTER TABLE symptoms ADD COLUMN "type_id" INTEGER REFERENCES "user_symptom"(id);
+ALTER TABLE symptoms ADD COLUMN type_id INTEGER;
+
+ALTER TABLE symptoms ADD CONSTRAINT s_us_fkey FOREIGN KEY (type_id) REFERENCES "user_symptom"(id) ON DELETE CASCADE;
 
 UPDATE symptoms SET type_id = user_symptom.id FROM user_symptom WHERE symptoms.type = user_symptom.type;
 
-ALTER TABLE symptoms ALTER COLUMN "type_id" SET NOT NULL;
+-- ALTER TABLE symptoms ALTER COLUMN "type_id" SET NOT NULL;
 
 ALTER TABLE symptoms DROP COLUMN type, DROP COLUMN user_id;
--- migrations/007.undo.alter_symptoms.sql
+++ migrations/007.undo.alter_symptoms.sql
@@ -1,5 +1,11 @@
-ALTER TABLE symptoms ADD COLUMN type varchar, ADD COLUMN user_id INTEGER references "users"(id);
+ALTER TABLE symptoms ADD COLUMN type varchar;
 
-ALTER TABLE symptoms ALTER COLUMN "type_id" DROP NOT NULL;
+ALTER TABLE symptoms ADD COLUMN user_id INTEGER references "users"(id) ON DELETE CASCADE;
 
-ALTER TABLE symptoms DROP COLUMN "type_id";
+UPDATE symptoms SET type = user_symptom.type FROM user_symptom WHERE symptoms.type_id = user_symptom.id;
+
+UPDATE symptoms SET user_id = user_symptom.user_id FROM user_symptom WHERE symptoms.type_id = user_symptom.id;
+
+ALTER TABLE symptoms DROP COLUMN type_id;
+
+
